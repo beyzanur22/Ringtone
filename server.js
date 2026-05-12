67dc235c64df5200092a6c8c0de7e2025e3ac372
Beyza Nur Özcan <167517048+beyzanur22@users.noreply.github.com>
2026-05-12 12:53:37 +0300

    şlmdf

--- server.js
+++ server.js
@@ -1722,7 +1722,7 @@ app.post("/send-notification", async (req, res) => {
   }
 
   const appId = bodyAppId || "9a255882-6fc4-43e6-af33-24f5f69642cf";
-  const restKey = bodyRestKey || "os_v2_app_tisvratpyrb6nlztet27nfscz7fc7ycdtssud74fxti6bqjgq56357mpy3i6w25tze5zicgyrtzcaoqmmszrtby7erxzhm5n62u2nci";
+  const restKey = bodyRestKey || "os_v2_app_tisvratpyrb6nlztet27nfscz4r5e6e2aj3ej6fa2utb4t477l72yiste6zff2qssfpszvtijhvugtawhkseqgjejb4ujp2mszgltyq";
 
   try {
     const { exec } = require("child_process");
