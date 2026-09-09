fed7de01d6a1fd905cadcf943acddc25ea47fd06
Beyza Nur Özcan <beyzanurozcan13@gmail.com>
2026-09-09 17:35:30 +0300

    klk

--- server.js
+++ server.js
@@ -1732,6 +1732,7 @@ app.use(async (req, res, next) => {
       req.path === "/peluna-music-privacy-policy.html" ||
       req.path === "/ploink-music-privacy-policy.html" ||
       req.path === "/nomii-music-privacy-policy.html" ||
+      req.path === "/nola-music-privacy-policy.html" ||
       req.path === "/child-safety-standards.html" ||
       req.path === "/loadtest") {
     return next();
@@ -6211,6 +6212,9 @@ app.get("/ploink-music-privacy-policy.html", (req, res) => {
 });
 app.get("/nomii-music-privacy-policy.html", (req, res) => {
   res.sendFile(path.join(__dirname, "nomii-music-privacy-policy.html"));
+});
+app.get("/nola-music-privacy-policy.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "nola-music-privacy-policy.html"));
 });
 app.get("/child-safety-standards.html", (req, res) => {
   res.sendFile(path.join(__dirname, "child-safety-standards.html"));
